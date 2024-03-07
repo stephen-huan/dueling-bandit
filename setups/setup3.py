5095a29e350bd0ff4ff0fa4ab68c6cc38d3f9f52
Stephen Huan <stephen03.huan@gmail.com>
2024-03-07 01:10:41 -0500

    refactor(setups/setup3): port to jax

--- setups/setup3.py
+++ setups/setup3.py
@@ -1,26 +1,34 @@
 from functools import partial
 
+import jax
 import numpy as np
+from equinox.nn import make_with_state
+from jax import random
 
 from bandit import algorithms, problems
 
-rng = np.random.default_rng(1)
+# enable int64/float64
+jax.config.update("jax_enable_x64", True)
+# set random seed
+rng = random.key(0)
 
 setup_name = "3"
 K = 10
 Ts = [*np.arange(1, 1000, 50)]
 trials = int(2e2)
 
+rng, *subkeys = random.split(rng, num=4)
 prob_list = {
-    "trivial": problems.RankingProblem(K, 100, rng=rng),
+    "trivial": make_with_state(problems.RankingProblem)(subkeys[0], K, 100),
     "easy": (
-        problems.CondorcetProblem(
-            problems.RandomProblem(K - 1, rng=rng), rng=rng
+        make_with_state(problems.CondorcetProblem)(
+            subkeys[1],
+            make_with_state(problems.RandomProblem)(subkeys[2], K - 1),
         )
     ),
 }
 alg_list = {
     "naive": algorithms.naive,
-    "dts+": partial(algorithms.d_ts, alpha=0.51, plus=True, rng=rng),
-    "vdb-rv-shared": partial(algorithms.vdb, rv=True, rng=rng),
+    "dts+": partial(algorithms.d_ts, alpha=0.51, plus=True),
+    "vdb-rv-shared": partial(algorithms.vdb, rv=True),
 }
